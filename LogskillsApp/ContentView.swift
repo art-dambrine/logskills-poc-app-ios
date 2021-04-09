01a6cf05c1d1496a4469fa7aa9c3356b27fe5ea0
art.dambrine <art.dambrine@gmail.com>
2021-04-09 08:59:23 +0200

    Initial Commit

---
+++ LogskillsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LogskillsApp
+//
+//  Created by Arthur Dambrine on 09/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LogskillsApp/LogskillsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogskillsAppApp.swift
+//  LogskillsApp
+//
+//  Created by Arthur Dambrine on 09/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct LogskillsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
