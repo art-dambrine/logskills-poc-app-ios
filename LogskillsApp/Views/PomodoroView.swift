97ae26e4b0d6ba377a2a2de44dfe862c84d1ef58
art.dambrine <art.dambrine@gmail.com>
2021-04-11 23:06:45 +0200

    minuteur programmable fonctionnel

--- LogskillsApp/Views/PomodoroView.swift
+++ LogskillsApp/Views/PomodoroView.swift
@@ -7,81 +7,154 @@
 
 import SwiftUI
 
-struct PomodoroView: View {
+enum TimerMode {
+    case running
+    case paused
+    case initial
+}
+
+class TimerManager: ObservableObject {
     
-    @State var seconds: Int = 0
-    @State var minutes: Int = 0
-    @State var hours: Int = 0
+    @Published var timerMode: TimerMode = .initial
     
-    @State var timer: Timer? = nil
-    @State var timerIsPaused: Bool = true
+    @Published var secondsLeft = UserDefaults.standard.integer(forKey: "timerlength")
+    
+    @Published var seconds: String = "00"
+    @Published var minutes: String = "00"
+    
+    var timer: Timer? = nil
     
     func startTimer(){
-        timerIsPaused = false
+        timerMode = .running
         // 1. Make a new timer
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true){ tempTimer in
-            // 2. Check time to add to H:M:S
-            if self.seconds == 59 {
-                self.seconds = 0
-                if self.minutes == 59 {
-                    self.minutes = 0
-                    self.hours = self.hours + 1
-                } else {
-                    self.minutes = self.minutes + 1
-                }
-            } else {
-                self.seconds = self.seconds + 1
+            // 2. Check time to change values
+            if self.secondsLeft == 0 {
+                self.restartTimer()
             }
+            
+            self.secondsLeft -= 1
+            
+            if (self.secondsLeft > 60) {
+                self.seconds = String(self.secondsLeft % 60)
+                if((self.secondsLeft / 60) < 10 ) {
+                    self.minutes = "0" + String(self.secondsLeft / 60)
+                } else {
+                    self.minutes = String(self.secondsLeft / 60)
+                }
+                
+            } else {
+                self.seconds = String(self.secondsLeft)
+                self.minutes = String("00")
+            }
+                        
         }
     }
     
     func stopTimer(){
-        timerIsPaused = true
+        timerMode = .paused
         timer?.invalidate()
         timer = nil
     }
     
     func restartTimer(){
-        self.hours = 0
-        self.minutes = 0
-        self.seconds = 0
+        timerMode = .initial
+        self.secondsLeft = UserDefaults.standard.integer(forKey: "timerlength")
+        self.minutes = "00"
+        self.seconds = "00"
     }
     
+    func setTimerlength(minutes: Int){
+        UserDefaults.standard.set(minutes, forKey: "timerlength")
+        secondsLeft = minutes
+    }
+    
+}
+
+
+struct PomodoroView: View {
+    
+    @ObservedObject var timerManager = TimerManager()
+    
+    @State var selectedPickerIndex = 1
+    let availableMinutes = Array(1...45)
+    
     var body: some View {
+        
         VStack {
-            Text("\(hours):\(minutes):\(seconds)")
             
-            if timerIsPaused {
-                HStack {
+            Text("Start Pomodoro")
+                .bold()
+                .font(.title2)
+            
+            Text("\(timerManager.minutes):\(timerManager.seconds)")
+                .font(.system(size: 60))
+                .padding(.top,10)
+            
+            if (timerManager.timerMode == .paused || timerManager.timerMode == .initial) {
+                VStack {
                     Button(action:{
-                        self.restartTimer()
+                        
+                        if timerManager.timerMode == .initial {
+                            print(self.selectedPickerIndex)
+                            self.timerManager.setTimerlength(minutes: self.availableMinutes[self.selectedPickerIndex]*60)
+                         }
+                        
+                        timerManager.startTimer()
+                        print("START")
+                        
+                    }){
+                        Image(systemName: "play.circle.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                            .frame(width: 90, height: 90)
+                            .foregroundColor(.pink)
+                        
+                    }
+                    .padding(.all)
+                    
+                    Button(action:{
+                        timerManager.restartTimer()
                         print("RESTART")
                     }){
                         Image(systemName: "backward.end.alt")
-                            .padding(.all)
-                    }
-                    .padding(.all)
-                    Button(action:{
-                        self.startTimer()
-                        print("START")
-                    }){
-                        Image(systemName: "play.fill")
-                            .padding(.all)
+                            .resizable()
+                            .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                            .frame(width: 20, height: 20)
+                            .foregroundColor(.red)
                     }
                     .padding(.all)
                 }
             } else {
                 Button(action:{
                     print("STOP")
-                    self.stopTimer()
+                    timerManager.stopTimer()
                 }){
-                    Image(systemName: "stop.fill")
-                        .padding(.all)
+                    Image(systemName: "pause.circle.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.pink)
                 }
                 .padding(.all)
             }
             
+            if timerManager.timerMode == .initial {
+                
+                Picker(selection: $selectedPickerIndex, label: Text("")){
+                    ForEach(0 ..< availableMinutes.count) {
+                        Text("\(self.availableMinutes[$0]) min")
+                    }
+                }
+                .labelsHidden()
+                
+            }
+            
+            Spacer()
+            
         }
+        .padding(.top,20)
+        
     }
 }
 
